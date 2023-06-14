87b4ee33904ad75c50e32b8687d7f4c2eda0a6ac
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-06-14 15:16:13 +0200

    Initial commit

---
+++ app/src/main/java/com/cursosant/inventorybase/addModule/model/AddRepository.kt
@@ -0,0 +1,13 @@
+package com.cursosant.inventorybase.addModule.model
+
+import com.cursosant.inventorybase.common.MyDataBase
+import com.cursosant.inventorybase.entities.Product
+
+
+class AddRepository {
+    private val myDataBase by lazy { MyDataBase.getInstance() }
+
+    fun addProduct(product: Product, callback: (isSuccess: Boolean) -> Unit){
+        callback(myDataBase.add(product))
+    }
+}
