3ff511aa8a6246612673cb2d907a0aa8fef4ce9a
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-03 11:00:46 +0200

    chore: Test onClick() Event from DialogFragment
    - section 5/64

--- app/src/test/java/com/cursosant/inventorybase/addModule/view/AddProductFragmentTest.kt
+++ app/src/test/java/com/cursosant/inventorybase/addModule/view/AddProductFragmentTest.kt
@@ -1,11 +1,14 @@
 package com.cursosant.inventorybase.addModule.view
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.fragment.app.testing.launchFragment
 import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.cursosant.inventorybase.R
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,6 +22,18 @@ class AddProductFragmentTest {
         scenario.moveToState(Lifecycle.State.RESUMED)
         scenario.onFragment { fragment ->
             assertThat(fragment.dialog, `is`(notNullValue()))
+        }
+    }
+
+    @Test
+    fun cancelDialog_isNullTest() {
+        val scenario = launchFragment<AddProductFragment>(themeResId = R.style.Theme_InventoryBase)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment { fragment ->
+            (fragment.dialog as? AlertDialog)?.let {
+                it.getButton(Dialog.BUTTON_NEGATIVE).performClick()
+                assertThat(fragment.dialog, `is`(nullValue()))
+            }
         }
     }
 }
